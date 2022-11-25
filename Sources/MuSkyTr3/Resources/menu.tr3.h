c22104dcb0526f087c2756a502dc906e834634f5
musesum <musesum@gmail.com>
2022-11-25 14:27:19 +0800

    multiviews menu.SW, menu.SE

--- Sources/MuSkyTr3/Resources/menu.tr3.h
+++ Sources/MuSkyTr3/Resources/menu.tr3.h
@@ -1,45 +1,46 @@
-menu {
-    view {
-        canvas (symbol "photo.artframe") {
-            tile (image "icon.shader.tile.png") {
-                mirror (symbol "arrowtriangle.left.and.line.vertical.and.arrowtriangle.right")
-                repeat (symbol "rectangle.grid.2x2" )
-                shift (image "icon.cell.scroll")
-                tilt  (image "icon.pen.tilt")
+menu  {
+    SW.view (depth 0, touch 0) {
+        canvas (symbol "photo.artframe", touch 0) {
+            tile (image "icon.shader.tile.png", touch 0) {
+                mirror (symbol "arrowtriangle.left.and.line.vertical.and.arrowtriangle.right", touch 0)
+                repeat (symbol "rectangle.grid.2x2" , touch 0)
+                shift (image "icon.cell.scroll", touch 0)
+                tilt  (image "icon.pen.tilt", touch 0)
             }
-            color(image "icon.pal.main") {
-                fade  (symbol "slider.horizontal.below.rectangle")
-                plane (symbol "square.3.layers.3d.down.right")
-                zero  (symbol "drop")
-                one   (symbol "drop.fill")
+            color(image "icon.pal.main", touch 0) {
+                fade  (symbol "slider.horizontal.below.rectangle", touch 0)
+                plane (symbol "square.3.layers.3d.down.right", touch 0)
+                zero  (symbol "drop", touch 0)
+                one   (symbol "drop.fill", touch 0)
             }
-            speed (image "icon.speed") {
-                fps (symbol "speedometer")
-                run (symbol "goforward")
+            speed (image "icon.speed", touch 0) {
+                fps (symbol "speedometer", touch 0)
+                run (symbol "goforward", touch 0)
             }
         }
-        brush (symbol "paintbrush.pointed") {
-            size  (symbol "circle.circle")
-            press (image "icon.pen.press")
-            tilt  (image "icon.pen.tilt")  //(symbol "angle")
+        brush (symbol "paintbrush.pointed", touch 0) {
+            size  (symbol "circle.circle", touch 0)
+            press (image "icon.pen.press", touch 0)
+            tilt  (image "icon.pen.tilt", touch 0)  //(symbol "angle", touch 0)
         }
-        cell (symbol "circle.grid.3x3") {
-            fade  (image "icon.cell.fade" )
-            ave   (image "icon.cell.ave"  )
-            melt  (image "icon.cell.melt" )
-            tunl  (image "icon.cell.tunl" )
-            zha   (image "icon.cell.zha"  )
-            slide (image "icon.cell.slide")
-            fred  (image "icon.cell.fred" )
+        cell (symbol "circle.grid.3x3", touch 0) {
+            fade  (image "icon.cell.fade" , touch 0)
+            ave   (image "icon.cell.ave"  , touch 0)
+            melt  (image "icon.cell.melt" , touch 0)
+            tunl  (image "icon.cell.tunl" , touch 0)
+            zha   (image "icon.cell.zha"  , touch 0)
+            slide (image "icon.cell.slide", touch 0)
+            fred  (image "icon.cell.fred" , touch 0)
         }
-        cam (symbol "camera") {
-            snap  (symbol "camera.shutter.button")
-            fake  (symbol "face.dashed")
-            real  (symbol "face.smiling")
-            face  (symbol "arrow.triangle.2.circlepath.camera")
-            mix   (symbol "slider.horizontal.below.rectangle")
+        cam (symbol "camera", touch 0) {
+            snap  (symbol "camera.shutter.button", touch 0)
+            fake  (symbol "face.dashed", touch 0)
+            real  (symbol "face.smiling", touch 0)
+            face  (symbol "arrow.triangle.2.circlepath.camera", touch 0)
+            mix   (symbol "slider.horizontal.below.rectangle", touch 0)
         }
     }
+    SE.view @ SW.view // copy southwest view
     model {
         canvas {
             tile  {
